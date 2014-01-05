46a27d984693294b63dd36b1914cdaa9eda0c30a
Yuya Unno <unnonouno@gmail.com>
2014-01-05 14:55:17 +0000

    Add pass test

--- src/oxelon/position_test.cpp
+++ src/oxelon/position_test.cpp
@@ -8,4 +8,8 @@ TEST(Position, to_str) {
   EXPECT_EQ("B4", Position(1, 3).to_str());
 }
 
+TEST(Position, pass_to_str) {
+  EXPECT_EQ("PS", Position(Position::POS_PASS).to_str());
+}
+
 }  // namespace oxelon
