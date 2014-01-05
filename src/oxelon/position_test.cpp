4538578680ff77fbcc5d8579ad0c7d279306ae4f
Yuya Unno <unnonouno@gmail.com>
2014-01-05 14:57:29 +0000

    Fix test for pass

--- src/oxelon/position_test.cpp
+++ src/oxelon/position_test.cpp
@@ -8,8 +8,10 @@ TEST(Position, to_str) {
   EXPECT_EQ("B4", Position(1, 3).to_str());
 }
 
-TEST(Position, pass_to_str) {
-  EXPECT_EQ("PS", Position(Position::POS_PASS).to_str());
+TEST(Position, pass) {
+  Position pass(Position::POS_PASS);
+  EXPECT_EQ("PS", pass.to_str());
+  EXPECT_TRUE(pass.pass());
 }
 
 }  // namespace oxelon
